288d8daa55ab1947d728111e9030cb3d844687af
Saket Saurabh <ssaurabh@apache.org>
2017-05-03 10:57:50 +0000

    Change the forever scripts to now use timeouts

--- nbt-master-forever.sh
+++ nbt-master-forever.sh
@@ -4,7 +4,7 @@ JAR=target/nbt-bundled-1.0-SNAPSHOT.jar
 CONF=src/main/resources/config-remote.json
 until java -jar $JAR -m -c $CONF; do
   echo "Master crashed with exit code $?.  Respawning in 1 second..."
-  pid=$(ps aux | grep nbt-visualizer | head -1 | awk '{print $2}')
-  kill $pid
+  #pid=$(ps aux | grep nbt-visualizer | head -1 | awk '{print $2}')
+  #kill $pid
   sleep 1
 done
--- nbt-visualizer/forever.sh
+++ nbt-visualizer/forever.sh
@@ -3,12 +3,7 @@
 SERVER=nbt-visualizer.js
 COUNTER=0
 while [ $COUNTER -lt 1 ]; do
-  # Check if nbt-visualizer.js is running or not.
-  N=$(ps aux | grep nbt-visualizer.js  | wc -l)
-  if [ $N -lt 2 ]; then
-    # Respawn the process
     echo "Respawing nbt-visualizer server process..."
-    node $SERVER
-  fi
-  sleep 3
+    timeout 1200 node $SERVER
+    sleep 1
 done
